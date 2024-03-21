267c978cb4900491ac1ef9e0584d544620789c43
TohidEq <TohidEq@gmail.com>
2024-03-21 21:07:00 +0330

    Create First Func and (gharz dadan moteghayer ha)

--- riverride/src/main.rs
+++ riverride/src/main.rs
@@ -1,16 +1,49 @@
 use std::io::{stdout, Stdout, Write};
 
 use crossterm::{
+    cursor::MoveTo,
     event, execute,
     style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-    ExecutableCommand,
+    terminal::{size, ScrollUp, SetSize},
+    ExecutableCommand, QueueableCommand,
 };
 
+struct World {
+    player_c: u16,
+    player_l: u16,
+}
+
+fn draw(mut sc: &Stdout, world: &World) {
+    sc.queue(MoveTo(world.player_c, world.player_l));
+    sc.queue(Print("P"));
+
+    sc.flush();
+}
+
 fn main() -> std::io::Result<()> {
-    // or using functions
+    //
+
+    // init screen
     let mut sc: Stdout = stdout();
 
-    sc.execute(Print("Styled text here."));
+    let (cols, rows) = size()?;
+
+    let mut world: World = World {
+        player_c: (cols / 2),
+        player_l: (rows - 1),
+    };
+
+    // init the game
+
+    loop {
+        // read and apply keyboard
+
+        // pysics
+
+        // draw
+
+        draw(&sc, &world);
+    }
 
     Ok(())
 }
