2f817277effcadec60228acc2990e6ee10c0cacc
TohidEq <TohidEq@gmail.com>
2024-03-21 20:24:59 +0330

    Install Crossterm

--- riverride/src/main.rs
+++ riverride/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdout, Stdout, Write};
+
+use crossterm::{
+    event, execute,
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+    ExecutableCommand,
+};
+
+fn main() -> std::io::Result<()> {
+    // or using functions
+    let mut sc: Stdout = stdout();
+
+    sc.execute(Print("Styled text here."));
+
+    Ok(())
 }
