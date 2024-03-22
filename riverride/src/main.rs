bb132e4523b28332065365516f25b283e1b62d8f
TohidEq <TohidEq@gmail.com>
2024-03-23 01:38:48 +0330

    Map generate randomly

--- riverride/src/main.rs
+++ riverride/src/main.rs
@@ -7,10 +7,15 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ScrollUp, SetSize},
     ExecutableCommand, QueueableCommand,
 };
-use std::io::{stdout, Result, Stdout, Write};
 use std::{io, thread, time};
+use std::{
+    io::{stdout, Result, Stdout, Write},
+    os::linux::raw::stat,
+};
 
 use crossterm::event::{poll, read, Event};
+
+use rand::Rng;
 
 struct World {
     maxC: u16,
@@ -19,7 +24,14 @@ struct World {
     player_l: u16,
     map: Vec<(u16, u16)>,
     died: bool,
+    nextStart: u16,
+    nextEnd: u16,
 }
+
+// 1~10 (low number = more chance)
+const MAP_SHIFT_CHANCE: u16 = 4;
+const MAP_GAP: u16 = 10;
+const MAP_GENERATE_RATE: u16 = 20;
 
 fn draw(mut sc: &Stdout, world: &World) -> std::io::Result<()> {
     sc.queue(Clear(crossterm::terminal::ClearType::All))?;
@@ -51,11 +63,48 @@ fn pysics(mut world: World) -> Result<World> {
     }
 
     // shift the map
+    let mut rng = rand::thread_rng();
+    // rng.gen_range(0..10)
+
     for l in (0..world.map.len() - 1).rev() {
         world.map[l + 1] = world.map[l]
     }
-    if world.map[0].0 > 2 {
-        world.map[0] = (world.map[0].0 - 1, world.map[0].1 + 1);
+
+    if (world.nextStart == world.map[0].0) {
+        //
+        let end = world.nextEnd - MAP_GAP;
+        let mut start = 0;
+        if end > MAP_GENERATE_RATE {
+            start = end - MAP_GENERATE_RATE
+        }
+        world.nextStart = rng.gen_range(start..end);
+    }
+    if (world.nextEnd == world.map[0].1) {
+        //
+        let mut end = world.maxC - 1;
+        let start = world.nextStart + MAP_GAP;
+        if start < ((world.maxC - 1) - MAP_GENERATE_RATE) {
+            end = start + MAP_GENERATE_RATE
+        }
+        world.nextEnd = rng.gen_range((start)..(end));
+    }
+
+    if rng.gen_range(0..10) > MAP_SHIFT_CHANCE {
+        if (rng.gen_range(0..2) == 1) {
+            if world.map[0].0 < world.nextStart {
+                world.map[0].0 += 1;
+            }
+            if world.map[0].0 > world.nextStart {
+                world.map[0].0 -= 1;
+            }
+        } else {
+            if world.map[0].1 < world.nextEnd {
+                world.map[0].1 += 1;
+            }
+            if world.map[0].1 > world.nextEnd {
+                world.map[0].1 -= 1;
+            }
+        }
     }
 
     Ok((world))
@@ -76,8 +125,10 @@ fn main() -> std::io::Result<()> {
         maxL: maxL,
         player_c: (maxC / 2),
         player_l: (maxL - 1),
-        map: vec![(maxC / 2 - 5, maxC / 2 + 5); maxL as usize],
+        map: vec![(maxC / 2 - MAP_GAP, maxC / 2 + MAP_GAP); maxL as usize],
         died: false,
+        nextStart: maxC / 2 - MAP_GAP - 1,
+        nextEnd: maxC / 2 + MAP_GAP + 1,
     };
 
     // init the game
@@ -150,3 +201,9 @@ fn main() -> std::io::Result<()> {
     sc.execute(Show)?;
     Ok(())
 }
+
+// NOTE: Random gen map shift
+// NOTE: Enemy
+// NOTE: Gas
+// NOTE: Gold
+// NOTE:
